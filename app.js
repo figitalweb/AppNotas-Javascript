85fecc1b773129ceb70cd3a6abd1fdd25534fd2b
Diego <die.11.91@gmail.com>
2020-07-05 16:31:30 -0300

    Se creo función createNote

--- app.js
+++ app.js
@@ -1,57 +1,38 @@
 // obtenemos los botones principales, agregar, editar y borrar
 let btnAddNote = document.getElementById('btnAddNote'),
     btnEditNote = document.getElementById('btnEditNote'),
-    btnDeleteNote = document.getElementById('btnDeleteNote')
+    btnDeleteNote = document.getElementById('btnDeleteNote');
 
+//Evento para crear la nota
+btnAddNote.addEventListener('click', createNote)
 
 //Funcion para crear un article con sus clases y contenido
-let createNote = (createTitle, createText) => {
-    let section = document.getElementsByClassName('notes')[0],
-        article = document.createElement('article'),
-        header = document.createElement('header'),
-        h2 = document.createElement('h2'),
-        div = document.createElement('div'),
-        p = document.createElement('p'),
-        Title = document.createTextNode(createTitle),
-        Text = document.createTextNode(createText)
+function createNote() {
+    let titleNote = document.getElementById('addTitleNote').value,
+        textNote = document.getElementById('addTextNote').value;
+    
+    let notes = {
+        titleNote,
+        textNote
+    }
 
-    h2.appendChild(Title)
-    header.appendChild(h2)
+    if (localStorage.getItem('notes') == null) {
+        let notesStorage = []
+        notesStorage.push(notes)
+        localStorage.setItem('notes', JSON.stringify(notesStorage))
+        
+    } else {
+        let notesStorage = JSON.parse(localStorage.getItem('notes'))
+        notesStorage.push(notes)
+        localStorage.setItem('notes', JSON.stringify(notesStorage))
+    }
+    
+    document.getElementById('formAddNotes').reset()
+}
 
-    p.appendChild(Text)
-    div.appendChild(p)
+function noteView() {
 
-    article.appendChild(header)
-    article.appendChild(div)
-
-    article.setAttribute('class', 'note')
-
-    return section.appendChild(article)
 }
 
 
-//Evento para crear la nota
-btnAddNote.addEventListener('click', () => {
-    let titleNote = document.getElementById('addTitleNote').value,
-        textNote = document.getElementById('addTextNote').value
-    
-    createNote(titleNote, textNote)
-
-    // if (document.getElementsByClassName('note').length > 0) {
-        
-    //     let articulos = document.getElementsByClassName('note')
-    //     for (let element of articulos) {
-    //         localStorage.setItem('ele', element)
-    //         let getele = localStorage.getItem('ele')
-    //         let v = document.getElementsByClassName('notes')[0]
-    //             console.log(getele)
-    //             console.log(v.append(getele))
-    //     }
-    // }
-
-    // console.log(localStorage.getItem('articleStorage'))
-
-    document.getElementById('addTitleNote').value = ""
-    document.getElementById('addTextNote').value = ""
-})
 
