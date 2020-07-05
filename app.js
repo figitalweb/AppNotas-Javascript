17f1c47d27487c43caae1f1b14765b4f167866ce
Diego <die.11.91@gmail.com>
2020-07-05 18:30:05 -0300

    Se termino funcion agregar article al DOM

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let btnAddNote = document.getElementById('btnAddNote'),
 btnAddNote.addEventListener('click', createNote)
 
 //Funcion para crear un article con sus clases y contenido
-function createNote() {
+function createNote(e) {
+    e.preventDefault();    
     let titleNote = document.getElementById('addTitleNote').value,
         textNote = document.getElementById('addTextNote').value;
     
@@ -16,7 +17,7 @@ function createNote() {
         textNote
     }
 
-    if (localStorage.getItem('notes') == null) {
+    if (localStorage.getItem('notes') === null) {
         let notesStorage = []
         notesStorage.push(notes)
         localStorage.setItem('notes', JSON.stringify(notesStorage))
@@ -27,12 +28,31 @@ function createNote() {
         localStorage.setItem('notes', JSON.stringify(notesStorage))
     }
     
+    noteView()
+
     document.getElementById('formAddNotes').reset()
 }
 
 function noteView() {
+    let notesStorage = JSON.parse(localStorage.getItem('notes'))
+    let notesContainer = document.getElementById('notes')
 
+    notesContainer.innerHTML = ''
+
+    for (let i = 0; i < notesStorage.length; i++){
+        let title = notesStorage[i].titleNote
+        let text = notesStorage[i].textNote
+            notesContainer.innerHTML += `<article class="note">
+                                            <header>
+                                                <h2>${title}</h2>
+                                            </header>
+                                            <div>
+                                                <p>${text}</p>
+                                            </div>
+                                        </article>`        
+    }
 }
 
-
-
+if (localStorage.getItem('notes') != null) {
+    noteView()
+}
